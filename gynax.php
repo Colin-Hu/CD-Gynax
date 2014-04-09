289a40435296c2a701a40b16b32812c09edc0f2c
Colin-Hu <colin.hu@outlook.com>
2014-04-08 20:04:23 -0500

    Update gynax.php

--- gynax.php
+++ gynax.php
@@ -4,6 +4,7 @@
 
 <?php
 $con=mysqli_connect("localhost","webbot","","planetary_interaction");
+//Opens connection to the appropriate database
 if (mysqli_connect_errno($con))
 {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -21,6 +22,7 @@ echo '<table border="1">';
 echo '<tr><td>Item ID</td><td>Item Name</td><td>Sell</td><td>Buy</td><td>Gynax Price</td></tr>';
 while($row = mysqli_fetch_array($result))
 {
+//Creates left side table of all available prices
 echo "<tr>";
 echo '<td>' . $row['typeID'] . '</td><td>' . $row['typeNAME'] . '</td><td>' . $row['min'] . '</td><td>' . $row['max'] . '</td><td>' . $row['max']*0.9 . '</td>';
 echo "</tr>";
@@ -33,6 +35,7 @@ echo "<br>";
 $timesincelast = (time()-strtotime($lastdate))/60;
 echo "Last Updated: " . (int)$timesincelast . " minutes ago";
 if ($timesincelast > 5)
+//Creates a flag in the database that requests for prices to be updated
 {
 echo '<form action="update.php" method="post">';
 echo '<input type="submit" value="Update Now">';
@@ -46,6 +49,7 @@ echo '<br>Prices recently updated please wait a few minutes.';
 echo "</div>";
 echo "<div style='float:left;'>";
 echo "<form action='records.php' method='post'>";
+//Area to enter records page to prevent basic users from accepting/denying orders
 echo "Insert Code to Access Orders: <input type='password' name='password'>";
 echo "<input type='submit'>";
 echo "</form>";
@@ -54,6 +58,7 @@ echo "<form action='quote.php' method='get'>";
 echo "<select type='text' name = 'item1'>";
 sort($items);
 for ($i=0;$i<count($items);$i++)
+//Creates drop down form for users to enter items and quantities and pushes them as gets
 {
 echo "<option value='" . $items[$i] . "'>" . $items[$i] . '</option>';
 }
